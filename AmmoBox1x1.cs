5f23e3616c722e0c2cb016ad5ba9427d4d9a2a5b
silviohmartins <silviohmartins@gmail.com>
2025-10-26 00:45:23 -0300

    Adicionar arquivos de projeto.

---
+++ AmmoBox1x1.cs
@@ -0,0 +1,51 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+
+namespace jeroAmmoBox1x1;
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class AmmoBox1x1(ISptLogger<AmmoBox1x1> logger, DatabaseServer databaseServer) : IOnLoad
+{
+    // Ammo Box Parent ID
+    private const string Parent = "543be5cb4bdc2deb348b4568";
+    private Dictionary<MongoId, TemplateItem>? _itemsDb;
+
+    public Task OnLoad()
+    {
+        var ammoBox = 0;
+        _itemsDb = databaseServer.GetTables().Templates.Items;
+
+        foreach (var item in _itemsDb.Where(item => item.Value.Parent.Equals(Parent)))
+        {
+            if (item.Value.Properties is null) continue;
+
+            // Change size to 1x1
+            if (item.Value.Properties.Width == 2 && item.Value.Properties.Height == 1)
+            {
+                item.Value.Properties.Width = 1;
+                ammoBox++;
+            }
+
+            // Change background color based on rarity
+            if (item.Value.Properties.RarityPvE == "Superrare")
+            {
+                item.Value.Properties.BackgroundColor = "violet";
+            }
+            else if (item.Value.Properties.RarityPvE == "rare")
+            {
+                item.Value.Properties.BackgroundColor = "blue";
+            }
+            else
+            {
+                item.Value.Properties.BackgroundColor = "yellow";
+            }
+        }
+
+        logger.Info($"[JERO] Ammo Boxes adjusted {ammoBox}!");
+        return Task.CompletedTask;
+    }
+}
---
+++ ModMetadata.cs
@@ -0,0 +1,20 @@
+﻿using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
+namespace SlimmerMagazines;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.jero.ammobox1x1";
+    public override string Name { get; init; } = "AmmoBox1x1";
+    public override string Author { get; init; } = "jero";
+    public override List<string>? Contributors { get; init; }
+    public override Version Version { get; init; } = new("1.0.1");
+    public override Range SptVersion { get; init; } = new("~4.0.1");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; }
+    public override string License { get; init; } = "MIT";
+}
